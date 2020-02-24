26581fd9311004edb0e8f7f5cd4ca0075ad570ad
Olya Kostenok <ola.kostenok@gmail.com>
2020-02-25 02:29:19 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if(array == undefined || array.length == 0){
+        return 0;
+    } else
+    {
+        array.sort((a , b) => a - b);
+        return array[0];
+    }  
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(array == undefined || array.length == 0){
+        return 0;
+    } else
+    {
+    array.sort((a , b) => b - a);
+    return array[0];
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(array == undefined || array.length == 0){
+        return 0;
+    } else
+    {
+    var sum = 0;
+    var average = 0;
+    array.map(function(item){
+    sum+=item;
+    })
+    average = sum/array.length;
+    return average;
+    }
 }
